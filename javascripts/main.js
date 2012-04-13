9d3280072fa95e5ac5e29b196d6282687ad86af0
Eddie Chan <cheyeungchan@gmail.com>
2012-04-13 16:00:03 -0400

    add more

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,4 +2,12 @@ console.log('This would be the main JS file.');
 
 jQuery(document).ready(function() {
     jQuery(".megamenu").megamenu();
+		jQuery(".cycle").cycle({
+			fit: 1,
+			fx: 'fade',
+			pager: '#smallnav', 
+			pause:   1, 
+			speed: 100,
+			timeout:  100 
+		});
 });
